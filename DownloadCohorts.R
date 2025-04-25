56a77a9ff878ffc3ae1637061dce974e30cef1dd
Erik Westlund <erikwestlund@hey.com>
2025-04-25 17:39:08 -0400

    cohorts: fix no specialty for in office; double-check all cohorts from Atlas.

--- DownloadCohorts.R
+++ DownloadCohorts.R
@@ -7,22 +7,22 @@ baseUrl <- "https://atlas-demo.ohdsi.org/WebAPI"
 cohortDefinitionSet <- ROhdsiWebApi::exportCohortDefinitionSet(
   baseUrl = baseUrl,
   cohortIds = c(
-    1792943, # 001_newly_diagnosed_t2dm.json
-    1792942, # 002_newly_diagnosed_t2dm_3yrs_continuous_obs.json
-    1792945, # 010_prevalent_t2dm.json
-    1792944, # 011_prevalent_t2dm_3yrs_continuous_obs.json
-    1792933, # 100_dr_screening_any.json [with specialty]
-    1792932, # 101_dr_screening_any_no_specialty.json
-    1792940, # 110_dr_screening_in_office.json
-    1792934, # 111_dr_screening_in_office_first_type.json
-    # 112_dr_screening_in_office_no_specialty.json
-    1792939, # 113_dr_screening_in_office_no_specialty_first_type.json [double check; it was duped]
-    1792941, # 120_dr_screening_telemedicine.json
-    1792936, # 121_dr_screening_telemedicine_first_type_excludes_in_office_with_specialty.json
-    1792937, # 122_dr_screening_telemedicine_first_type_excludes_in_office_without_specialty.json
-    1792938, # 130_dr_screening_ai.json
-    1792930, # 131_dr_screening_ai_first_type_excludes_in_office_with_specialty.json
-    1792931 # 132_dr_screening_ai_first_type_excludes_in_office_without_specialty.json
+    1792943,
+    1792942,
+    1792945,
+    1792944,
+    1792933,
+    1792932,
+    1792940,
+    1792939,
+    1792934,
+    1792935,
+    1792941,
+    1792936,
+    1792937,
+    1792938,
+    1792930,
+    1792931
   ),
   generateStats = TRUE
 )
@@ -37,8 +37,9 @@ cohortDefinitionSet <- cohortDefinitionSet |>
       cohortId == 1792933 ~ "DR Screening, Any",
       cohortId == 1792932 ~ "DR Screening, Any (No Specialty)",
       cohortId == 1792940 ~ "DR Screening, In Office",
+      cohortId == 1792939 ~ "DR Screening, In Office (No Specialty)",
       cohortId == 1792934 ~ "DR Screening, In Office (First Type)",
-      cohortId == 1792939 ~ "DR Screening, In Office (No Specialty, First Type)",
+      cohortId == 1792935 ~ "DR Screening, In Office (No Specialty, First Type)",
       cohortId == 1792941 ~ "DR Screening, Telemedicine",
       cohortId == 1792936 ~ "DR Screening, Telemedicine (First Type, Excludes In Office, With Specialty)",
       cohortId == 1792937 ~ "DR Screening, Telemedicine (First Type, Excludes In Office, Without Specialty)",
@@ -55,8 +56,9 @@ cohortDefinitionSet <- cohortDefinitionSet |>
       cohortId == 1792933 ~ 100,
       cohortId == 1792932 ~ 101,
       cohortId == 1792940 ~ 110,
-      cohortId == 1792934 ~ 111,
-      cohortId == 1792939 ~ 113,
+      cohortId == 1792939 ~ 111,
+      cohortId == 1792934 ~ 112,
+      cohortId == 1792935 ~ 113,
       cohortId == 1792941 ~ 120,
       cohortId == 1792936 ~ 121,
       cohortId == 1792937 ~ 122,
